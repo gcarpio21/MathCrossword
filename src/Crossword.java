5a6c86e533d8d6ce3d4b0ebc613a8411701626fc
gabriel <gabriel.carpio.99@gmail.com>
2024-10-07 16:25:38 +0200

    commit

--- src/Crossword.java
+++ src/Crossword.java
@@ -5,6 +5,8 @@ import com.google.common.annotations.VisibleForTesting;
 public class Crossword {
   private static final int WIDTH = 9;
   private static final int HEIGHT = 11;
+  //Set<Variables> variables;
+  private char[] variables;
   private List<String> equations;
   private String[] crossword = new String[HEIGHT];
 
@@ -12,8 +14,12 @@ public class Crossword {
     this.equations = equations;
   }
 
-  public static Crossword parseCrossword(String[] crossword) {
+  /*public static Crossword parseCrossword(String[] crossword) {
     String regex = "([0-9_](\\+|\\-|\\*|\\/)[0-9_]=[0-9_])";
+    //crossword = replaceUnderscores(crossword);
+    for (int i =0, j=0; i<WIDTH && j<HEIGHT; i++, j++){
+
+    }
     parseHorizontalEquations(crossword);
     parseVerticalEquations(crossword);
 
@@ -22,18 +28,32 @@ public class Crossword {
   }
 
   private static List<String> parseVerticalEquations(String[] crossword) {
-    String[] wordChars = new String[WIDTH];
-    int variables = 1;
-    for (String word : crossword) {
-      wordChars = word.split("");
-      for (String wordChar : wordChars) {
-        if (wordChar == "_") {
-          wordChar = "x_" + variables;
-        }
-      }
-    }
+
 
     return null;
+  }
+
+  private static String[] replaceUnderscores(String[] crossword) {
+    String[] wordChars;
+    String row;
+    int variables = 1;
+
+    for (int rowNo = 0; rowNo < HEIGHT; rowNo++) {
+      StringBuilder noUnderscoresRow = new StringBuilder();
+      row = crossword[rowNo];
+      wordChars = row.split("");
+      for (String wordChar : wordChars) {
+        if (wordChar == "_") {
+          noUnderscoresRow.append("x_" + variables);
+        } else {
+          noUnderscoresRow.append(wordChar);
+        }
+      }
+
+      crossword[rowNo] = noUnderscoresRow.toString();
+
+    }
+    return crossword;
   }
 
   private static List<String> parseHorizontalEquations(String[] crossword) {
@@ -56,5 +76,6 @@ public class Crossword {
   private void setCrossword(String crossword) {
     this.crossword = formatCrossword(crossword);
   }
+  */
 
 }
--- test/CrosswordTests.java
+++ test/CrosswordTests.java
@@ -8,7 +8,7 @@ public class CrosswordTests {
 
   @Test
   @DisplayName("formatExampleCrossword")
-  void formatCrossword() throws Exception {
+  void testFormatCrossword() throws Exception {
     String crossword =
         "1+_=6   _" + "  / /   +" + "_-_=3 _ 2" + "* = = / =" + "_ 1 _*4=_" + "=     =  " +
             "8 5-_=1 3" + "  + +   *" + "8/_=_   1" + "  = =   =" + "  _ 8-_=_" ;
@@ -32,5 +32,32 @@ public class CrosswordTests {
     String[] actual = (String[]) method.invoke(null, crossword);
 
     assertArrayEquals(expected, actual);
+  }
+
+  void testReplaceUnderscores() throws Exception {
+    String[] crossword =
+        {"1+_=6   _",
+            "  / /   +",
+            "_-_=3 _ 2",
+            "* = = / =",
+            "_ 1 _*4=_",
+            "=     =  ",
+            "8 5-_=1 3",
+            "  + +   *",
+            "8/_=_   1",
+            "  = =   =",
+            "  _ 8-_=_"};
+    String[] expected =
+        {"1+x_1=6   x_2",
+            "  / /   +",
+            "x_3-x_4=3 x_4 2",
+            "* = = / =",
+            "x_4 1 x_5*4=x_6",
+            "=     =  ",
+            "8 5-x_7=1 3",
+            "  + +   *",
+            "8/x_8=x_9   1",
+            "  = =   =",
+            "  x_10 8-x_11=x_12"};
   }
 }
