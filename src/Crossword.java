e1261aae64374040870ae97e3a02c30544c76625
gabriel <gabriel.carpio.99@gmail.com>
2024-10-07 14:03:14 +0200

    commit

--- src/Crossword.java
+++ src/Crossword.java
@@ -1,31 +1,60 @@
 import java.util.ArrayList;
 import java.util.List;
+import com.google.common.annotations.VisibleForTesting;
 
 public class Crossword {
   private static final int WIDTH = 9;
-  private static final int HEIGHT = 10;
+  private static final int HEIGHT = 11;
   private List<String> equations;
+  private String[] crossword = new String[HEIGHT];
 
   public Crossword(List<String> equations) {
     this.equations = equations;
   }
 
   public static Crossword parseCrossword(String[] crossword) {
-
+    String regex = "([0-9_](\\+|\\-|\\*|\\/)[0-9_]=[0-9_])";
+    parseHorizontalEquations(crossword);
+    parseVerticalEquations(crossword);
 
     Crossword crosswordObj = new Crossword(null);
     return null;
   }
 
+  private static List<String> parseVerticalEquations(String[] crossword) {
+    String[] wordChars = new String[WIDTH];
+    int variables = 1;
+    for (String word : crossword) {
+      wordChars = word.split("");
+      for (String wordChar : wordChars) {
+        if (wordChar == "_") {
+          wordChar = "x_" + variables;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private static List<String> parseHorizontalEquations(String[] crossword) {
+    return null;
+  }
+
+
   private static String[] formatCrossword(String crossword) {
     String[] formattedCrossword = new String[HEIGHT];
-    char[] crosswordArray = crossword.toCharArray();
+
     for (int i = 0; i < HEIGHT; i++) {
-      for (int j = 0; i < WIDTH; j++) {
-        formattedCrossword[i] = formattedCrossword[i] + crosswordArray[j];
-      }
+      int start = i * WIDTH;
+      int end = start + WIDTH;
+      formattedCrossword[i] = crossword.substring(start, end);
     }
 
     return formattedCrossword;
   }
+
+  private void setCrossword(String crossword) {
+    this.crossword = formatCrossword(crossword);
+  }
+
 }
--- test/CrosswordTests.java
+++ test/CrosswordTests.java
@@ -1,2 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+
 public class CrosswordTests {
+
+  @Test
+  @DisplayName("formatExampleCrossword")
+  void formatCrossword() throws Exception {
+    String crossword =
+        "1+_=6   _" + "  / /   +" + "_-_=3 _ 2" + "* = = / =" + "_ 1 _*4=_" + "=     =  " +
+            "8 5-_=1 3" + "  + +   *" + "8/_=_   1" + "  = =   =" + "  _ 8-_=_" ;
+
+    String[] expected =
+        {"1+_=6   _",
+            "  / /   +",
+            "_-_=3 _ 2",
+            "* = = / =",
+            "_ 1 _*4=_",
+            "=     =  ",
+            "8 5-_=1 3",
+            "  + +   *",
+            "8/_=_   1",
+            "  = =   =",
+            "  _ 8-_=_"};
+
+    Method method = Crossword.class.getDeclaredMethod("formatCrossword", String.class);
+    method.setAccessible(true);
+
+    String[] actual = (String[]) method.invoke(null, crossword);
+
+    assertArrayEquals(expected, actual);
+  }
 }
