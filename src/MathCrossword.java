c712ad0e1c228ba9bde7791f4c7610b9ec351ee1
gabriel <gabriel.carpio.99@gmail.com>
2024-10-08 17:30:23 +0200

    commit

--- src/MathCrossword.java
+++ src/MathCrossword.java
@@ -1,3 +1,14 @@
+import org.sosy_lab.common.ShutdownManager;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.log.BasicLogManager;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.java_smt.SolverContextFactory;
+import org.sosy_lab.java_smt.api.BooleanFormulaManager;
+import org.sosy_lab.java_smt.api.FormulaManager;
+import org.sosy_lab.java_smt.api.IntegerFormulaManager;
+import org.sosy_lab.java_smt.api.SolverContext;
+
 /**
  * This program solves the given math crossword using an SMT solver via the JavaSMT API.
  *
@@ -54,11 +65,24 @@
 
 public class MathCrossword {
 
+  public static void main(String[] args) throws InvalidConfigurationException {
+    Configuration config = Configuration.fromCmdLineArguments(args);
+    LogManager logger = BasicLogManager.create(config);
+    ShutdownManager shutdown = ShutdownManager.create();
+
+    // SolverContext is a class wrapping a solver context.
+    // Solver can be selected either using an argument or a configuration option
+    // inside `config`.
+    SolverContext context = SolverContextFactory.createSolverContext(
+        config, logger, shutdown.getNotifier(), SolverContextFactory.Solvers.Z3);
+
+    FormulaManager fmgr = context.getFormulaManager();
+
+    BooleanFormulaManager bmgr = fmgr.getBooleanFormulaManager();
+    IntegerFormulaManager imgr = fmgr.getIntegerFormulaManager();
 
 
-   public static void parseCrossword(Crossword crossword) {
-    crossword.parseCrossword();
-    }
+  }
 
 
 }
